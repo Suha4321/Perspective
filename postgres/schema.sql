3df0bc5c71e688a8ddce18b6c328afe143fde01b
Suha <suha4321@gmail.com>
2019-09-20 17:34:51 -0400

    adding ingestion and sql schema file

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,16 +1,63 @@
-import psycopg2
+---Performance table
+--CREATE SCHEMA IF NOT EXISTS mortgage AUTHORIZATION joe;
 
-connection = psycopg2.connect(host = '127.0.0.1', database = 'test', user = 'postgres', password = 'new')
+DROP TABLE  IF EXISTS  Performance;
+DROP TABLE  IF EXISTS  Acquisition;
+DROP TABLE  IF EXISTS  First_Time_Home;
+DROP TABLE  IF EXISTS  Channel_Home;
+DROP TABLE  IF EXISTS  Property_Type;
+DROP TABLE  IF EXISTS  Agency_Type;
 
-cursor = connection.cursor()
 
-cursor.execute('''INSERT INTO playground (equip_id, type, color, location) values (0, 'see-saw', 'red', 'north')''')
+CREATE TABLE Performance(
+            loan_seq_no                 Text,
+            cur_interest_rate           FLOAT,
+            mon_to_maturity             DATE,
+            agency_id                   Text
+--            PRIMARY KEY( loan_seq_no )
+            );
 
-connection.commit() # Important!
+---Acquisition table
 
-cursor.execute('''SELECT * FROM playground''')
+CREATE TABLE  Acquisition(
+            loan_seq_no Text ,
+            original_interest_rate          FLOAT,
+            origination_date                DATE,
+            number_of_borrowers             INT,
+            credit_score                    INT,
+            first_time_homebuyer_flag       Text,
+            loan_purpose                    Text,
+            property_type                   Text,
+            number_of_units                 INT,
+            occupancy_status                Text,
+            property_state                  Text,
+            postal_code                     Text,
+            product_type                    Text,
+            co_borrower_credit_score        INT,
+            mortgage_insurance_type         Text,
+            relocation_mortgage_indicator   Text,
+            agency_id                       Text
+            );
 
-print(cursor.fetchall())
+---Create reference tables
 
-cursor.close()
-connection.close()
+CREATE TABLE First_Time_Home(
+            first_time_home_id              TEXT    PRIMARY KEY,
+            first_time_home_desc            TEXT
+            );
+
+
+CREATE TABLE Channel_Home(
+            channel_id                      TEXT    PRIMARY KEY,
+            channel_desc                    TEXT
+            );
+
+CREATE TABLE Property_Type(
+            Property_type_id                TEXT    PRIMARY KEY,
+            Property_type_desc              TEXT
+            );
+
+CREATE TABLE Agency_Type(
+            agency_id                        TEXT    PRIMARY KEY,
+            agency_desc                      TEXT
+            );
